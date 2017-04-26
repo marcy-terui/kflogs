bddd8231d7f80a99b547ddbb67cc2c749c6f8581
Marcy <marcy9114@gmail.com>
2017-04-26 17:01:15 +0900

    update register script

--- register.py
+++ register.py
@@ -5,7 +5,8 @@ import kflogs
 os.system("python setup.py install")
 
 os.system("pandoc README.md --from=markdown --to=rst > README.txt")
-os.system("python setup.py sdist upload")
+os.system("python setup.py sdist")
+os.system("twine upload dist/*")
 os.remove('README.txt')
 
 time.sleep(60)
